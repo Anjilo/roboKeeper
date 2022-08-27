e9dc1ade0ebf4dbd13fade7b493b67cc9a2dc59d
drasaev <mitya1130@gmail.com>
2022-08-27 16:30:23 +0500

    copied from disk

---
+++ Assets/Map/Objects.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Objects" tilewidth="405" tileheight="390" tilecount="24" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="121" height="121" source="../Drawing/Objects/Background.png"/>
+ </tile>
+ <tile id="1">
+  <image width="121" height="121" source="../Drawing/Objects/Background2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="121" height="121" source="../Drawing/Objects/Background3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="121" height="121" source="../Drawing/Objects/Background4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="121" height="121" source="../Drawing/Objects/Background5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="122" height="85" source="../Drawing/Objects/Box.png"/>
+ </tile>
+ <tile id="6">
+  <image width="94" height="84" source="../Drawing/Objects/ButtonLeft.png"/>
+ </tile>
+ <tile id="7">
+  <image width="94" height="84" source="../Drawing/Objects/ButtonRight.png"/>
+ </tile>
+ <tile id="8">
+  <image width="89" height="96" source="../Drawing/Objects/Crane_part1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="12" height="362" source="../Drawing/Objects/Crane_part2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="88" height="60" source="../Drawing/Objects/Crane_part3.png"/>
+ </tile>
+ <tile id="11">
+  <image width="405" height="45" source="../Drawing/Objects/Floor.png"/>
+ </tile>
+ <tile id="12">
+  <image width="86" height="13" source="../Drawing/Objects/Pipe1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="86" height="13" source="../Drawing/Objects/Pipe2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="86" height="13" source="../Drawing/Objects/Pipe3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="86" height="13" source="../Drawing/Objects/Pipe4.png"/>
+ </tile>
+ <tile id="16">
+  <image width="99" height="258" source="../Drawing/Objects/Robot.png"/>
+ </tile>
+ <tile id="17">
+  <image width="126" height="155" source="../Drawing/Objects/RobotSmall.png"/>
+ </tile>
+ <tile id="19">
+  <image width="390" height="37" source="../Drawing/Objects/Roof_part2.png"/>
+ </tile>
+ <tile id="20">
+  <image width="177" height="132" source="../Drawing/Objects/Table.png"/>
+ </tile>
+ <tile id="21">
+  <image width="59" height="390" source="../Drawing/Objects/Wall.png"/>
+ </tile>
+ <tile id="23">
+  <image width="50" height="43" source="../Drawing/Objects/RoofCorner.png"/>
+ </tile>
+ <tile id="25">
+  <image width="390" height="43" source="../Drawing/Objects/Roof_part1.png"/>
+ </tile>
+ <tile id="27">
+  <image width="390" height="43" source="../Drawing/Objects/Roof_empty.png"/>
+ </tile>
+</tileset>
